ebad60cfb2b089fc3cd55c7de228395c13b68004
Samuel Perroni <samuel_perroni@outlook.com>
2024-08-19 16:08:53 -0300

    adaptações aula

---
+++ aula_python_rpa/aula_python_rpa/main.py
@@ -0,0 +1,19 @@
+import re
+from web_scraping.webscraping_bovespa import Bovespa
+from web_scraping.webscraping_coinmarketcap import CoinMarketCap
+from const import *
+
+get_values_from_bovespa = Bovespa(BOVESPA_URL, CSV_PATH)
+get_values_from_bovespa.get_values_b3()
+
+get_bitcoin_value = CoinMarketCap(COINMARKETCAP_URL)
+price = get_bitcoin_value.get_bitcoin_price()
+
+if price == 'Não foi possível encontrar o preço do Bitcoin.' or price == 'Falha ao acessar o site.':
+    print('ERRO: Não foi possível coletar o preço do Bitcoin.')
+    print(f'Não foi possível coletar o preço do Bitcoin.\nTente novamente mais tarde ou verifique se houve mudanças no site.\nError: {price}')
+else:
+    price = re.sub(r'[^0-9,.]', '', price)
+    print('Atualização de preço do Bitcoin')
+    print(f'O preço do Bitcoin hoje é R${price[0:price.index('.')]}.')
+    
---
+++ aula_python_rpa/aula_python_rpa/web_scraping/webscraping_coinmarketcap.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+class CoinMarketCap:
+    def __init__(self, url):
+        self.url = url
+        
+    def get_bitcoin_price(self) -> str:        
+        # Requisição HTTP para a página
+        response = requests.get(self.url)
+        
+        # Verifica se a requisição foi bem-sucedida
+        if response.status_code == 200:
+            # Cria o objeto BeautifulSoup
+            soup = BeautifulSoup(response.text, 'html.parser')
+            
+            # Encontra o elemento que contém o preço
+            price_element = soup.find('div', class_='sc-65e7f566-0 DDohe flexStart alignBaseline')
+            
+            if price_element:
+                # Extrai o texto do elemento e remove espaços extras
+                price = price_element.get_text(strip=True)
+                return price
+            else:
+                return "Não foi possível encontrar o preço do Bitcoin."
+        else:
+            return "Falha ao acessar o site."
