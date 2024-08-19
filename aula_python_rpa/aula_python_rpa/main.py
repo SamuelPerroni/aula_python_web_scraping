b949dc58c786dbb03e3b33894d075660cca622f1
Samuel Perroni <samuel_perroni@outlook.com>
2024-08-19 20:50:52 -0300

    frist commit

--- aula_python_rpa/aula_python_rpa/main.py
+++ aula_python_rpa/aula_python_rpa/main.py
@@ -2,14 +2,14 @@ import re
 from web_scraping.webscraping_bovespa import Bovespa
 from web_scraping.webscraping_coinmarketcap import CoinMarketCap
 from const import *
-
+'''
 get_values_from_bovespa = Bovespa(BOVESPA_URL, CSV_PATH)
 get_values_from_bovespa.get_values_b3()
-
+'''
 get_bitcoin_value = CoinMarketCap(COINMARKETCAP_URL)
 price = get_bitcoin_value.get_bitcoin_price()
 
-if price == 'Não foi possível encontrar o preço do Bitcoin.' or price == 'Falha ao acessar o site.':
+if price == ('Não foi possível encontrar o preço do Bitcoin.' or 'Falha ao acessar o site.'):
     print('ERRO: Não foi possível coletar o preço do Bitcoin.')
     print(f'Não foi possível coletar o preço do Bitcoin.\nTente novamente mais tarde ou verifique se houve mudanças no site.\nError: {price}')
 else:
